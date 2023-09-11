97e005ee4a5b75b8eb32397624961b718c739b7a
mats pekkari <mats.pekkari@iths.se>
2023-09-11 10:27:25 +0200

    make 4 lines

--- enums/Program.cs
+++ enums/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Hello från Cloud23!");
             Console.WriteLine("Detta är version 2");
+            Console.WriteLine("rad 3 ");
+            Console.WriteLine("Rad 4");
         }
     }
 }
